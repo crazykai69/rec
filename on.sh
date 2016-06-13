92e67c62a54652ec57b32af21b61d79fe5c71584
Kai Merklein <mail@kai-merklein.de>
2016-06-14 00:10:05 +0200

    Sometimes no text

--- on.sh
+++ on.sh
@@ -4,6 +4,7 @@ cd /opt/rec
 case "$(sudo /opt/vc/bin/tvservice -s | grep -c 0x120002)" in
 1) echo "HDMI is off, turning on"
    sudo /opt/vc/bin/tvservice -p
+   sleep 1
    ;;
 *) echo "HDMI is already on"
    ;;
@@ -14,9 +15,7 @@ touch /opt/rec/tvon
 case "$(pidof hello_font.bin | wc -w)" in
 0) /opt/rec/hello_font.bin > hf.log 2>hf.err &
    ;;
-1) # all ok
-   ;;
-*) kill $(pidof hello_font.bin | awk '{print $1}')
+*) # all ok
    ;;
 esac
 
