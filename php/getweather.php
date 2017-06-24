0ba29b5b8219b75cd263ac7370acaa07de0b041c
Kai Merklein <mail@kai-merklein.de>
2017-06-24 20:33:34 +0200

    weather

--- php/getweather.php
+++ php/getweather.php
@@ -6,6 +6,8 @@ shell_exec( 'sudo wget http://192.168.178.70/livedata.htm' );
 
 $htmlLoaded = $doc->loadHTMLFile( 'livedata.htm' );
 
+$url = "http://www.kai-merklein.de/avrtl/updweather.php?station=1";
+
 if( $htmlLoaded )
 {
   echo "html loaded<hr>";
@@ -13,14 +15,32 @@ if( $htmlLoaded )
 
   foreach( $atts as $att )
   {
-//    echo $att->nodeValue, PHP_EOL;
-    echo $att->getAttribute( "name" ) . ": ";
-    echo $att->getAttribute( "value" ) . "<hr>";
+    $name  = $att->getAttribute( "name" );
+    $value = $att->getAttribute( "value" );
+	
+	echo "$name: $value<br>";
+	
+	if( $name == "inTemp" ) $url = $url . "&it=". $value;
+	if( $name == "outTemp" ) $url = $url . "&ot=". $value;
+	if( $name == "inHumi" ) $url = $url . "&ih=". $value;
+	if( $name == "outHumi" ) $url = $url . "&oh=". $value;
+	if( $name == "windir" ) $url = $url . "&wd=". $value;
+	if( $name == "avgwind" ) $url = $url . "&wf=". $value;
+	if( $name == "gustspeed" ) $url = $url . "&gs=". $value;
+	if( $name == "solarrad" ) $url = $url . "&so=". $value;
+	if( $name == "rainofhourly" ) $url = $url . "&rh=". $value;
+	if( $name == "rainofdaily" ) $url = $url . "&rd=". $value;
+	if( $name == "RelPress" ) $url = $url . "&pr=". $value;
+	
   }
+  echo $url;
+  
+  echo file_get_contents( $url );
 }
 else echo ("failed to load html");
 echo "</body></html>";
 
 shell_exec( 'sudo rm livedata* ' );
+shell_exec( 'sudo rm updweather* ' );
 
 ?>
