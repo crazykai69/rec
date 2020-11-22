b9f812d9e2433ffb5340e55e2956dee7c7b7b8ca
Kai Merklein <mail@kai-merklein.de>
2020-11-22 17:10:16 +0100

    unwanted change

--- run_hello_font_from_rc.local.sh
+++ run_hello_font_from_rc.local.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 cd /home/pi
+<<<<<<< Updated upstream
 while 1
+=======
+while /home/pi/hello_font.bin >> /home/pi/hello_font.log
+>>>>>>> Stashed changes
 do
   /home/pi/hello_font.bin
   sleep 2
